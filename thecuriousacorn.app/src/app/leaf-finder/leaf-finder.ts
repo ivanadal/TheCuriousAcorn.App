59fd5e037fd9721350e8955542a9c834cc528078
Ivana Dalcekovic <50738448+ivanadal@users.noreply.github.com>
2026-03-03 16:47:33 -0800

    Initial

---
+++ thecuriousacorn.app/src/app/leaf-finder/leaf-finder.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'leaf-finder',
+  imports: [],
+  templateUrl: './leaf-finder.html',
+  styleUrl: './leaf-finder.css',
+})
+export class LeafFinder {}
