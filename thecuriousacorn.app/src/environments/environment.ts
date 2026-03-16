32fdbedd8cd90c9cc7a13d0ab85228f0db8e74bd
Ivana Dalcekovic <50738448+ivanadal@users.noreply.github.com>
2026-03-15 19:58:25 -0700

    Create environment.ts

---
+++ thecuriousacorn.app/src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:5002/api',
+  apiBaseUrl: 'http://localhost:5002', // for static files like audio
+};
+
+
+
+
+
+
+
+
